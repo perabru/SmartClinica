7f0e3cedbd72127c28c4c838476ce9cc0763367c
Bruno Michel <brunomichel00@gmail.com>
2018-09-23 08:16:36 -0300

    230918@perabru- Added the classes CADASTRO, CONEXAOBD and PERMISSOES

---
+++ app/src/main/java/smcl/com/brunopera/smartclinica/Cadastro.java
@@ -0,0 +1,122 @@
+package smcl.com.brunopera.smartclinica;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Cadastro {
+
+    private String nome;
+    /*  private Calendar data = Calendar.getInstance();
+      private String dataAtual = DateFormat.getDateInstance().format(data.getTime());*/
+    private String dataAniversario;
+    private String estadoCivil;
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String CEP;
+    private String telefone;
+    private String celular;
+    private String profissao;
+    private String email;
+
+
+
+
+    public Cadastro() {
+
+    }
+
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getDataAniversario() {
+        return dataAniversario;
+    }
+
+    public void setDataAniversario(String dataAniversario) {
+        this.dataAniversario = dataAniversario;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
---
+++ app/src/main/java/smcl/com/brunopera/smartclinica/ConexaoBD.java
@@ -0,0 +1,140 @@
+package smcl.com.brunopera.smartclinica;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class ConexaoBD {
+
+    //FiREBASE - vai no nó raiz do firebase
+    static DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
+    //adiciona a child cadastro em um objeto
+    static DatabaseReference cadastro = referencia.child("cadastro");
+
+    public static String uniquePK = cadastro.push().getKey();
+
+    Cadastro cad = new Cadastro();
+    String[] parts;
+
+    //String newPK;
+
+       /* public void quebrarString (String teste){
+            String [] partes = teste.split("-");
+             newPK = partes[2];
+        }*/
+
+    public void inserirNomeBD(String nome) {
+
+        cad.setNome(nome);
+        //cad.setDataAtual(cad.getDataAtual());
+        cadastro.child(uniquePK).child("Dados Iniciais").setValue(cad);
+    }
+
+    public void inserirIdadeBD(ArrayList<String> idade) {
+
+        //corta o array para gravar apenas o numero da idade no banco
+        String[] str = new String[idade.size()];
+        idade.toArray(str);
+        parts = str[0].split("\\s+");
+
+
+        cadastro.child(uniquePK).child("Idade").setValue(Integer.parseInt(parts[0]));
+
+    }
+
+    public void inserirDataNasc(String dataNascimento) {
+
+        cad.setDataAniversario(dataNascimento);
+        cadastro.child(uniquePK).child("Data Nascimento").setValue(cad);
+
+    }
+
+    public void inserirEstadoCivil(String estadoCivil) {
+
+        cad.setEstadoCivil(estadoCivil);
+        cadastro.child(uniquePK).child("Estado Civil").setValue(cad);
+
+    }
+
+    public void inserirEndereco(String endereco) {
+
+        cad.setEndereco(endereco);
+        cadastro.child(uniquePK).child("Endereço").setValue(cad);
+
+    }
+
+    public void inserirBairro(String bairro) {
+
+        cad.setEndereco(bairro);
+        cadastro.child(uniquePK).child("Bairro").setValue(cad);
+
+    }
+
+
+    public void inserirCidade(String cidade) {
+
+        cad.setEndereco(cidade);
+        cadastro.child(uniquePK).child("Cidade").setValue(cad);
+
+    }
+
+    public void inserirCEP(String CEP) {
+
+        cad.setEndereco(CEP);
+        cadastro.child(uniquePK).child("CEP").setValue(cad);
+
+    }
+
+    public void inserirTelefone(String telefone) {
+
+        cad.setTelefone(telefone);
+        cadastro.child(uniquePK).child("Telefone").setValue(cad);
+
+    }
+
+
+    public void inserirCelular(String celular) {
+
+        cad.setCelular(celular);
+        cadastro.child(uniquePK).child("Celular").setValue(cad);
+
+    }
+
+    public void inserirProfissao(String profissao) {
+
+        cad.setProfissao(profissao);
+        cadastro.child(uniquePK).child("Profissao").setValue(cad);
+
+    }
+
+    public void inserirEmail(String email) {
+
+        email.replaceAll("arroba","@");
+        cad.setEmail(email.replaceAll("\\s+",""));
+        cadastro.child(uniquePK).child("Email").setValue(cad);
+
+    }
+
+    public void inserirData() {
+        Calendar data = Calendar.getInstance();
+        String dataAtual = DateFormat.getDateInstance().format(data.getTime());
+
+        cadastro.child(uniquePK).child("Data").setValue(dataAtual);
+
+    }
+
+    public void inserirLocalizacao(String localizacaoAtual) {
+
+
+        cadastro.child(uniquePK).child("Localização Atual").setValue(localizacaoAtual);
+
+    }
+
+
+}
